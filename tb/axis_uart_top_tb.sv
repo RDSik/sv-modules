ebf4be0ff0362a3eb9f050f7f68702fa4e402b7a
Dmitry <dmitryryabickov@yandex.ru>
2025-03-14 23:09:36 +0300

    Upd tb

--- tb/axis_uart_top_tb.sv
+++ tb/axis_uart_top_tb.sv
@@ -8,22 +8,15 @@ localparam CLK_FREQ   = 27_000_000;
 localparam BAUD_RATE  = 115_200;
 localparam DATA_WIDTH = 8;
 localparam CLK_PER    = 2;
+localparam SIM_TIME   = 50000;
 
 axis_uart_top_if dut_if();
 
 environment env;
 
 initial begin
-    env = new(dut_if, CLK_PER, CLK_FREQ, BAUD_RATE, DATA_WIDTH);
-    fork
-        env.clk_gen();
-        env.run();
-    join_any
-    `ifdef VERILATOR
-    $finish();
-    `else
-    $stop();
-    `endif
+    env = new(dut_if, CLK_PER, CLK_FREQ, BAUD_RATE, DATA_WIDTH, SIM_TIME);
+    env.run();
 end
 
 initial begin
--- tb/environment.sv
+++ tb/environment.sv
@@ -9,56 +9,69 @@ class environment;
     int baud_rate;
     int data_width;
     int clk_per;
+    int sim_time;
 
-    function new(virtual axis_uart_top_if dut_if, int clk_per, int clk_freq, int baud_rate, int data_width);
+    function new(virtual axis_uart_top_if dut_if, int clk_per, int clk_freq, int baud_rate, int data_width, int sim_time);
         this.dut_if     = dut_if;
         this.clk_per    = clk_per;
         this.clk_freq   = clk_freq;
         this.baud_rate  = baud_rate;
         this.data_width = data_width;
+        this.sim_time   = sim_time;
     endfunction
 
     task run();
         begin
-            rst_gen();
-            data_gen(10);
+            fork
+                clock_gen();
+                reset_gen($urandom_range(1, 10));
+                data_gen();
+            join_none
+            #sim_time;
             $display("Stop simulation at: %g ns\n", $time);
+            `ifdef VERILATOR
+            $finish();
+            `else
+            $stop();
+            `endif
         end
     endtask
 
-    task data_gen(int n);
+    task data_gen();
         int ratio = clk_freq/baud_rate;
+        logic [7:0] tmp_data;
         begin
-            repeat (n) begin
+            wait(dut_if.arstn_i);
+            forever begin
+                tmp_data = $urandom_range(0, (2**data_width)-1);
+                $display("Data to transmit: 8'b%b - 8'h%h\n", tmp_data, tmp_data);
                 dut_if.uart_rx_i = 1'b0;
                 $display("Start bit detected at: %g ns\n", $time);
-                #((ratio/2)*clk_per);
+                #((ratio/2)*clk_per+clk_per);
                 $display("Data transmission start in %g ns\n", $time);
                 for (int i = 0; i < data_width; i++) begin
-                    /* verilator lint_off WIDTHTRUNC */
-                    dut_if.uart_rx_i = $urandom_range(0, 1);
-                    /* verilator lint_on WIDTHTRUNC */
-                    $display("%d bit detected in %g ns\n", i, $time);
+                    dut_if.uart_rx_i = tmp_data[i];
+                    $display("%d bit detected in %g ns and equal: %b\n", i, $time, tmp_data[i]);
                     #(ratio*clk_per);
                 end
                 @(posedge dut_if.clk_i);
                 dut_if.uart_rx_i = 1'b1;
-                $display("Stop bit detected in %g ns", $time);
+                $display("Stop bit detected in %g ns\n", $time);
                 #(ratio*clk_per);
             end
         end
     endtask
 
-    task rst_gen();
+    task reset_gen(int delay);
         begin
             dut_if.arstn_i = 1'b0;
-            $display("Reset at %g ns\n.", $time);
-            @(posedge dut_if.clk_i);
+            repeat (delay) @(posedge dut_if.clk_i);
             dut_if.arstn_i = 1'b1;
+            $display("Reset done at %g ns\n.", $time);
         end
     endtask
 
-    task clk_gen();
+    task clock_gen();
         begin
             dut_if.clk_i = 1'b0;
             forever begin
