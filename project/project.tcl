fa1d884426d97dddb57c7284d18ecf7c89dd5a79
Dmitry <dmitryryabickov@yandex.ru>
2025-03-15 13:45:48 +0300

    Upd project file

--- project/project.tcl
+++ project/project.tcl
@@ -1,10 +1,22 @@
-create_project -name axis_uart_top -dir project -pn GW2A-LV18PG256C8/I7 -device_version C -force
-add_file ../../rtl/axis_if.sv
-add_file ../../rtl/axis_uart_top.sv
-add_file ../../rtl/axis_uart_tx.sv
-add_file ../../rtl/axis_uart_rx.sv
-add_file ../axis_uart_top.sdc
-add_file ../axis_uart_top.cst
-set_option -top_module axis_uart_top
-set_option -verilog_std sysv2017
+set top      "axis_uart_top"
+set part     "GW2A-LV18PG256C8/I7"
+set dev_ver  "C"
+set language "sysv2017"
+
+set rtl_dir       "../../rtl"
+set constrain_dir ".."
+
+create_project -name $top -dir project -pn $part -device_version $dev_ver -force
+
+add_file $rtl_dir/axis_if.sv
+add_file $rtl_dir/axis_uart_top.sv
+add_file $rtl_dir/axis_uart_tx.sv
+add_file $rtl_dir/axis_uart_rx.sv
+
+add_file $constrain_dir/axis_uart_top.sdc
+add_file $constrain_dir/axis_uart_top.cst
+
+set_option -top_module $top
+set_option -verilog_std $language
+
 run all
