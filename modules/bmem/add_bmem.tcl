958fcfff00083b85d1996475dbd4beb4ed3faf23
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2025-06-11 00:32:52 +0300

    Update add_bmem.tcl

--- modules/bmem/add_bmem.tcl
+++ modules/bmem/add_bmem.tcl
@@ -1,9 +1,10 @@
 set path [file dirname [info script]]
 
 set xil_defaultlib "
+    $path/rtl/brom.sv
+    $path/rtl/bram_dp.sv
     $path/rtl/bram_dp_2clk.sv
     $path/rtl/bram_true_dp.sv
-    $path/rtl/bram_dp.sv
 "
 
 add_files -norecurse $xil_defaultlib
