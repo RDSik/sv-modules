87ad5bcb4b943ffb3600969d32483d12b53dbfc1
RDSik <dmitryryabickov@yandex.ru>
2025-06-12 01:00:13 +0300

    udp

--- modules/axis_spi/rtl/axis_spi_pkg.svh
+++ modules/axis_spi/rtl/axis_spi_pkg.svh
@@ -16,28 +16,30 @@ package axis_spi_pkg;
 
     typedef struct packed {
         logic [23:0]           rsrvd;
-        logic [DATA_WIDTH-1:0] tx_data;
+        logic [DATA_WIDTH-1:0] data;
     } spi_tx_data_reg_t;
 
     typedef struct packed {
         logic [23:0]           rsrvd;
-        logic [DATA_WIDTH-1:0] rx_data;
+        logic [DATA_WIDTH-1:0] data;
     } spi_rx_data_reg_t;
 
     typedef struct packed {
-        spi_rx_data_reg_t     rx_data;
-        spi_tx_data_reg_t     tx_data;
+        spi_rx_reg_t          rx;
+        spi_tx_reg_t          tx;
         spi_mode_reg_t        mode;
         spi_clk_divider_reg_t clk_divider;
     } uart_regs_t;
 
-    localparam int SPI_CLK_DIVIDER_REG_ADDR = 0;
+    localparam int SPI_CONTROL_REG_ADDR = 0;
 
-    localparam int SPI_MODE_REG_ADDR = SPI_CLK_DIVIDER_REG_ADDR + $bits(spi_clk_divider_reg_t) / 32;
+    localparam int SPI_CLK_DIVIDER_REG_ADDR = 4*(SPI_CONTROL_REG_ADDR + 1);
 
-    localparam int SPI_TX_DATA_REG_ADDR = SPI_MODE_REG_ADDR + $bits(spi_mode_reg_t) / 32;
+    localparam int SPI_MODE_REG_ADDR = 4*(SPI_CLK_DIVIDER_REG_ADDR + $bits(spi_clk_divider_reg_t)/32);
 
-    localparam int SPI_RX_DATA_REG_ADDR = SPI_TX_DATA_REG_ADDR + $bits(spi_tx_data_reg_t) / 32;
+    localparam int SPI_TX_DATA_REG_ADDR = 4*(SPI_MODE_REG_ADDR + $bits(spi_mode_reg_t)/32);
+
+    localparam int SPI_RX_DATA_REG_ADDR = 4*(SPI_TX_DATA_REG_ADDR + $bits(spi_tx_data_reg_t)/32);
 
 endpackage
 
