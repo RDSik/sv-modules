c4510a817724eaf6b73be231c991930b684f772f
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2025-08-26 13:42:04 +0300

    Update bram_true_dp.sv

--- modules/common/rtl/bram_true_dp.sv
+++ modules/common/rtl/bram_true_dp.sv
@@ -2,10 +2,10 @@
 module bram_true_dp #(
     parameter int BYTE_NUM   = 4,
     parameter int BYTE_WIDTH = 8,
-    parameter int ADDR_WIDTH = 32,
     parameter int MEM_DEPTH  = 8192,
     parameter     MODE       = "NO_CHANGE",
     parameter     MEM_FILE   = "",
+    parameter int ADDR_WIDTH = $clog2(MEM_DEPTH),
     parameter int MEM_WIDTH  = BYTE_NUM * BYTE_WIDTH
 ) (
     input  logic                  a_clk_i,
