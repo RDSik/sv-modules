7833d67574b5f0f92e132ee3dda3c3e8da89146e
RDSik <dmitryryabickov@yandex.ru>
2025-09-27 19:18:05 +0300

    fix

--- modules/interface/add_if.tcl
+++ modules/interface/add_if.tcl
@@ -2,15 +2,14 @@ set path [file dirname [info script]]
 
 if {$xilinx == 1} {
     set xil_defaultlib "
-        $path/rtl/axis_if.sv
         $path/rtl/axil_if.sv
-        $path/rtl/apb_if.sv
+        $path/rtl/axis_if.sv
         $path/rtl/spi_if.sv
     "
 
     add_files -norecurse $xil_defaultlib
 } elseif {$gowin == 1} {
+    add_file $path/rtl/axil_if.sv
     add_file $path/rtl/axis_if.sv
-    add_file $path/rtl/apb_if.sv
     add_file $path/rtl/spi_if.sv
 }
