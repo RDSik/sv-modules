fdf0d2437633c3a1a9afa6caa0e03fa83890fa57
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2025-10-18 18:09:45 +0300

    Update uart.c

--- modules/uart/sdk/uart.c
+++ modules/uart/sdk/uart.c
@@ -9,29 +9,41 @@
 #include "xparameters.h"
 #include "sleep.h"
 
+#define BASE_ADDR 0x43c00000
+
 int main() {
     init_platform();
 
     while(1) {
         xil_printf("UART start test\n");
 
-        uart_regs_t *regs_pt = 0x43c00000;
+        uart_regs_t uart_regs;
 
-        regs_pt->control.rx_reset    = 0;
-        regs_pt->control.tx_reset    = 0;
-        regs_pt->control.parity_even = 0;
-        regs_pt->control.parity_odd  = 0;
-        regs_pt->clk_divider         = 50e6/115200;
-        regs_pt->tx.data             = 0x4D; // ASCII - M
+        uart_regs->control.rx_reset    = 0;
+        uart_regs->control.tx_reset    = 0;
+        uart_regs->control.parity_even = 0;
+        uart_regs->control.parity_odd  = 0;
+        uart_regs->clk_divider         = 50e6/115200;
+        uart_regs->tx.data             = 0x4D; // ASCII - M
 
-        xil_printf("clk_divider = %0d\n\r", regs_pt->clk_divider);
-        xil_printf("tx_data = %0d\n\r", regs_pt->tx.data);
-        xil_printf("parity_even = %0d\n\r", regs_pt->control.parity_even);
-        xil_printf("parity_odd = %0d\n\r", regs_pt->control.parity_odd);
-        xil_printf("rx_reset = %0d\n\r", regs_pt->control.rx_reset);
-        xil_printf("tx_reset = %0d\n\r", regs_pt->control.tx_reset);
-        xil_printf("rx_data = %0d\n\r", regs_pt->rx.data);
-        xil_printf("\n\r");
+        Xil_Out32(BASE_ADDR + 0, uart_regs->control);
+        Xil_Out32(BASE_ADDR + 4, uart_regs->clk_divider);
+        Xil_Out32(BASE_ADDR + 8, uart_regs->tx.data);
+
+        uint32_t reg_num;
+        uint32_t addr_offset;
+        uint32_t rd_data;
+
+        addr_offset = sizeof(uint32_t);
+        reg_num = sizeof(uart_regs) / addr_offset;
+        xil_printf("Number of regs %0d\n\r", reg_num);    
+
+        for (uint32_t i = 0; i < regs_num*addr_offset; i += addr_offset) {
+            rd_data = Xil_In32(BASE_ADDR + i);
+            xil_printf("The data at 0x%x is 0x%x \n\r", BASE_ADDR + i, rd_data);
+            xil_printf("\n\r");
+        }
+
         usleep(100000);
 
         xil_printf("UART stop test\n");
