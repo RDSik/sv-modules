43b2b5468c8ab093f42c2572557e9ab1d6f89441
RDSik <dmitryryabickov@yandex.ru>
2025-11-04 20:50:09 +0300

    fix

--- modules/fifo/add_fifo.tcl
+++ modules/fifo/add_fifo.tcl
@@ -18,6 +18,7 @@ if {$xilinx == 1} {
     add_files -fileset sim_1 $xil_defaultlib
 } elseif {$gowin == 1} {
     add_file $path/rtl/async_fifo.sv
+    add_file $path/rtl/fifo_wrap.sv
     add_file $path/rtl/axis_fifo_wrap.sv
     add_file $path/rtl/rd_ptr_empty.sv
     add_file $path/rtl/sync_fifo.sv
