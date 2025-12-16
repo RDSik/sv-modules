18d921f8e60d63cc57b57baa8f47f6337e737911
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2025-12-16 12:19:37 +0300

    Update sdk.tcl

--- project/pz7020starlite/sdk.tcl
+++ project/pz7020starlite/sdk.tcl
@@ -21,7 +21,7 @@ createbsp -name $bsp -hwproject $hw_project -proc $cpu -os standalone
 
 createapp -name $app -app {Empty Application} -hwproject $hw_project -bsp $bsp -proc $cpu -os standalone -lang C
 
-configapp app $app build-config debug
+configapp -app $app build-config debug
 
 createbsp -name $device_tree -hwproject $hw_project -proc $cpu -os device_tree
 
