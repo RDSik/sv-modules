0066ffd464da6fc19475f8bb342b791b97d15923
RDSik <dmitryryabickov@yandex.ru>
2026-02-07 13:47:05 +0300

    add device tree

--- sdk.tcl
+++ sdk.tcl
@@ -18,7 +18,7 @@ file delete -force $sdk_dir/$device_tree
 
 setws $sdk_dir
 
-repo -set "Zynq-Linux/device-tree-xlnx-xilinx-v2019.1"
+repo -set "device-tree-xlnx-xilinx-v2019.1"
 
 createhw -name $hw_project -hwspec $sdk_dir/$syn_top.hdf
 
