44ab4dd3da966c15a6c5a5d742a7f39d54f74b75
RDSik <dmitryryabickov@yandex.ru>
2026-02-19 01:25:19 +0300

    change mmcm in clk_manager

--- modules/common/rtl/clk_manager.sv
+++ modules/common/rtl/clk_manager.sv
@@ -22,72 +22,49 @@ module clk_manager #(
     logic clk0_out;
     logic clk1_out;
 
-    MMCME2_ADV #(
-        .BANDWIDTH           ("OPTIMIZED"),  // Jitter programming (OPTIMIZED, HIGH, LOW)
-        .CLKOUT4_CASCADE     ("FALSE"),      // Cascade CLKOUT4 counter with CLKOUT6 (FALSE, TRUE)
-        .COMPENSATION        ("ZHOLD"),      // ZHOLD, BUF_IN, EXTERNAL, INTERNAL
-        .STARTUP_WAIT        ("FALSE"),      // Delays DONE until MMCM is locked (FALSE, TRUE)
-        .DIVCLK_DIVIDE       (1),            // Master division value (1-106)
-        .CLKFBOUT_MULT_F     (CLK_MULT),     // Multiply value for all CLKOUT (2.000-64.000).
-        .CLKFBOUT_PHASE      (0.000),        // Phase offset in degrees of CLKFB (-360.000-360.000).
+    MMCME2_BASE #(
+        .BANDWIDTH         ("OPTIMIZED"),  // Jitter programming (OPTIMIZED, HIGH, LOW)
+        .CLKOUT4_CASCADE   ("FALSE"),      // Cascade CLKOUT4 counter with CLKOUT6 (FALSE, TRUE)
+        .REF_JITTER1       (0.0),          // Reference input jitter in UI (0.000-0.999).
+        .STARTUP_WAIT      ("FALSE"),      // Delays DONE until MMCM is locked (FALSE, TRUE)
+        .DIVCLK_DIVIDE     (1),            // Master division value (1-106)
+        .CLKFBOUT_MULT_F   (CLK_MULT),     // Multiply value for all CLKOUT (2.000-64.000).
+        .CLKFBOUT_PHASE    (0.000),        // Phase offset in degrees of CLKFB (-360.000-360.000).
         // CLKIN_PERIOD: Input clock period in ns to ps resolution (i.e. 33.333 is 30 MHz).
-        .CLKIN1_PERIOD       (CLK_PERIOD),
+        .CLKIN1_PERIOD     (CLK_PERIOD),
         // CLKOUT0_DIVIDE - CLKOUT6_DIVIDE: Divide amount for CLKOUT (1-128)
-        .CLKOUT0_DIVIDE_F    (CLK0_DIVIDE),
-        .CLKOUT1_DIVIDE      (CLK1_DIVIDE),
+        .CLKOUT0_DIVIDE_F  (CLK0_DIVIDE),
+        .CLKOUT1_DIVIDE    (CLK1_DIVIDE),
         // CLKOUT0_DUTY_CYCLE - CLKOUT6_DUTY_CYCLE: Duty cycle for CLKOUT outputs (0.01-0.99).
-        .CLKOUT0_DUTY_CYCLE  (0.500),
-        .CLKOUT1_DUTY_CYCLE  (0.500),
+        .CLKOUT0_DUTY_CYCLE(0.500),
+        .CLKOUT1_DUTY_CYCLE(0.500),
         // CLKOUT0_PHASE - CLKOUT6_PHASE: Phase offset for CLKOUT outputs (-360.000-360.000).
-        .CLKOUT0_PHASE       (0.000),
-        .CLKOUT1_PHASE       (0.000),
-        // USE_FINE_PS: Fine phase shift enable (TRUE/FALSE)
-        .CLKFBOUT_USE_FINE_PS("FALSE"),
-        .CLKOUT0_USE_FINE_PS ("FALSE"),
-        .CLKOUT1_USE_FINE_PS ("FALSE")
+        .CLKOUT0_PHASE     (0.000),
+        .CLKOUT1_PHASE     (0.000)
     ) i_mmcm (
-        .CLKOUT0     (clk0_out),      // 1-bit output: CLKOUT0
-        .CLKOUT0B    (),              // 1-bit output: Inverted CLKOUT0
-        .CLKOUT1     (clk1_out),      // 1-bit output: CLKOUT1
-        .CLKOUT1B    (),              // 1-bit output: Inverted CLKOUT1
-        .CLKOUT2     (),              // 1-bit output: CLKOUT2
-        .CLKOUT2B    (),              // 1-bit output: Inverted CLKOUT2
-        .CLKOUT3     (),              // 1-bit output: CLKOUT3
-        .CLKOUT3B    (),              // 1-bit output: Inverted CLKOUT3
-        .CLKOUT4     (),              // 1-bit output: CLKOUT4
-        .CLKOUT5     (),              // 1-bit output: CLKOUT5
-        .CLKOUT6     (),              // 1-bit output: CLKOUT6
-        // DRP Ports: 16-bit (each) output: Dynamic reconfiguration ports
-        .DO          (),              // 16-bit output: DRP data
-        .DRDY        (),              // 1-bit output: DRP ready
-        // Dynamic Phase Shift Ports: 1-bit (each) output: Ports used for dynamic phase shifting of the outputs
-        .PSDONE      (),              // 1-bit output: Phase shift done
+        .CLKOUT0  (clk0_out),      // 1-bit output: CLKOUT0
+        .CLKOUT0B (),              // 1-bit output: Inverted CLKOUT0
+        .CLKOUT1  (clk1_out),      // 1-bit output: CLKOUT1
+        .CLKOUT1B (),              // 1-bit output: Inverted CLKOUT1
+        .CLKOUT2  (),              // 1-bit output: CLKOUT2
+        .CLKOUT2B (),              // 1-bit output: Inverted CLKOUT2
+        .CLKOUT3  (),              // 1-bit output: CLKOUT3
+        .CLKOUT3B (),              // 1-bit output: Inverted CLKOUT3
+        .CLKOUT4  (),              // 1-bit output: CLKOUT4
+        .CLKOUT5  (),              // 1-bit output: CLKOUT5
+        .CLKOUT6  (),              // 1-bit output: CLKOUT6
         // Feedback Clocks: 1-bit (each) output: Clock feedback ports
-        .CLKFBOUT    (clk_fbin),      // 1-bit output: Feedback clock
-        .CLKFBOUTB   (),              // 1-bit output: Inverted CLKFBOUT
+        .CLKFBOUT (clk_fbin),      // 1-bit output: Feedback clock
+        .CLKFBOUTB(),              // 1-bit output: Inverted CLKFBOUT
         // Status Ports: 1-bit (each) output: MMCM status ports
-        .CLKFBSTOPPED(),              // 1-bit output: Feedback clock stopped
-        .CLKINSTOPPED(),              // 1-bit output: Input clock stopped
-        .LOCKED      (locked_o),      // 1-bit output: LOCK
+        .LOCKED   (locked_o),      // 1-bit output: LOCK
         // Clock Inputs: 1-bit (each) input: Clock inputs
-        .CLKIN1      (clk_i),         // 1-bit input: Primary clock
-        .CLKIN2      ('0),            // 1-bit input: Secondary clock
+        .CLKIN1   (clk_i),         // 1-bit input: Clock
         // Control Ports: 1-bit (each) input: MMCM control ports
-        .CLKINSEL    ('0),            // 1-bit input: Clock select, High=CLKIN1 Low=CLKIN2
-        .PWRDWN      ('0),            // 1-bit input: Power-down
-        .RST         (rst_i),         // 1-bit input: Reset
-        // DRP Ports: 7-bit (each) input: Dynamic reconfiguration ports
-        .DADDR       ('0),            // 7-bit input: DRP address
-        .DCLK        ('0),            // 1-bit input: DRP clock
-        .DEN         ('0),            // 1-bit input: DRP enable
-        .DI          ('0),            // 16-bit input: DRP data
-        .DWE         ('0),            // 1-bit input: DRP write enable
-        // Dynamic Phase Shift Ports: 1-bit (each) input: Ports used for dynamic phase shifting of the outputs
-        .PSCLK       ('0),            // 1-bit input: Phase shift clock
-        .PSEN        ('0),            // 1-bit input: Phase shift enable
-        .PSINCDEC    ('0),            // 1-bit input: Phase shift increment/decrement
+        .PWRDWN   ('0),            // 1-bit input: Power-down
+        .RST      (rst_i),         // 1-bit input: Reset
         // Feedback Clocks: 1-bit (each) input: Clock feedback ports
-        .CLKFBIN     (clk_fbin_bufg)  // 1-bit input: Feedback clock
+        .CLKFBIN  (clk_fbin_bufg)  // 1-bit input: Feedback clock
     );
 
     BUFG i_bufg_fbin (
