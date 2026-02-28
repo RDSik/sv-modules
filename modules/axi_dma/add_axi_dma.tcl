6b6b714db519ac8fd31069d94d0ab0ac8e2a76a4
RDSik <dmitryryabickov@yandex.ru>
2026-02-28 20:16:06 +0300

    add axi_dma ip for simulation

---
+++ modules/axi_dma/add_axi_dma.tcl
@@ -0,0 +1,14 @@
+set path [file dirname [info script]]
+
+if {$xilinx == 1} {
+    set xci_defaultlib "
+        $path/ip/axi_dma_sim/axi_dma_sim.xci
+    "
+    add_files -norecurse $xci_defaultlib
+    
+    set xil_defaultlib "
+        $path/rtl/axi_dma_wrap.sv
+    "
+
+    add_files -norecurse $xil_defaultlib
+}
