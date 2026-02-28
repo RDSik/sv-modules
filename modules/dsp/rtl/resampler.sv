9330404d99b3d45c47f560912f2982b92b1fa7f5
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2026-02-28 18:30:12 +0300

    Update resampler.sv

--- modules/dsp/rtl/resampler.sv
+++ modules/dsp/rtl/resampler.sv
@@ -66,14 +66,14 @@ module resampler #(
                         end
                     end
                     INTERP: begin
+                        int_tdata <= '0;
                         if (int_cnt_last) begin
                             int_cnt    <= '0;
-                            int_tvalid <= s_axis.tvalid;
-                            int_tdata  <= s_axis.tdata;
+                            int_tvalid <= '0;
+                            state      <= IDLE;
                         end else begin
                             int_cnt    <= int_cnt_next;
                             int_tvalid <= '1;
-                            int_tdata  <= '0;
                         end
                     end
                     default: state <= IDLE;
