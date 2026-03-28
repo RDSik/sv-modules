7bebb06b6acf4c7091cfe153a8ead19e517f385b
RDSik <dmitryryabickov@yandex.ru>
2026-03-28 13:12:01 +0300

    add non complex data

--- modules/dsp/tb/write_iq_to_file.m
+++ modules/dsp/tb/write_iq_to_file.m
@@ -1,6 +1,12 @@
-function write_iq_to_file(file_name, format, data)
-    file_data(1, :) = real(data);
-    file_data(2, :) = imag(data);
+function write_iq_to_file(file_name, format, data, complex)
+    if (complex)
+        tmp_data(1, :) = real(data);
+        tmp_data(2, :) = imag(data);
+    else
+        tmp_data = data;
+    end
+
+    file_data = tmp_data;
 
     fid = fopne(file_name, 'wb');
     fwite(fid, file_data(:), format);
