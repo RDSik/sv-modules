1ed29655e5428e3eaf80ac0e40c4d0cc03db279d
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2026-05-08 14:54:53 +0300

    Update axis_reg.sv

--- modules/common/rtl/axis_reg.sv
+++ modules/common/rtl/axis_reg.sv
@@ -3,18 +3,20 @@ module axis_reg (
     axis_if.master m_axis
 );
 
+    logic enable = m_axis.tready | ~m_axis.tvalid;
+
     always_ff @(posedge m_axis.clk_i) begin
         if (m_axis.rst_i) begin
             m_axis.tvalid <= '0;
             m_axis.tlast  <= '0;
             m_axis.tdata  <= '0;
-        end else if (m_axis.tready | ~m_axis.tvalid) begin
+        end else if (enable) begin
             m_axis.tvalid <= s_axis.tvalid;
             m_axis.tlast  <= s_axis.tlast;
             m_axis.tdata  <= s_axis.tdata;
         end
     end
 
-    assign s_axis.tready = m_axis.tready | ~m_axis.tvalid;
+    assign s_axis.tready = enable;
 
 endmodule
