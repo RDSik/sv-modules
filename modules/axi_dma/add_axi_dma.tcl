fba21c85382a98cbf5791b47355a2b6fcfe61aea
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2026-06-30 13:26:58 +0300

    Update add_axi_dma.tcl

--- modules/axi_dma/add_axi_dma.tcl
+++ modules/axi_dma/add_axi_dma.tcl
@@ -6,8 +6,14 @@ if {$xilinx == 1} {
     set xil_defaultlib "
         $path/rtl/axi_dma_test_wrap.sv
         $path/rtl/axi_dma_pkg.svh
-        $path/tb/axi_dma_tb.sv
     "
 
     add_files -norecurse $xil_defaultlib
+
+    set xil_defaultlib "
+        $path/tb/axi_dma_tb.sv
+        $path/tb/axi_dma_class.svh
+    "
+    add_files -fileset sim_1 $xil_defaultlib
+
 }
