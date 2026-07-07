5ad679e8f3fcc56b070bb756c592004081ae7987
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2026-07-07 15:44:54 +0300

    Update axis_reg_slice.sv

--- modules/common/rtl/axis_reg_slice.sv
+++ modules/common/rtl/axis_reg_slice.sv
@@ -13,20 +13,10 @@ module axis_reg_slice (
             m_axis.tvalid <= '0;
             m_axis.tlast  <= '0;
             m_axis.tdata  <= '0;
-            m_axis.tkeep  <= '0;
-            m_axis.tid    <= '0;
-            m_axis.tdest  <= '0;
-            m_axis.tstrb  <= '0;
-            m_axis.tuser  <= '0;
         end else if (enable) begin
             m_axis.tvalid <= s_axis.tvalid;
             m_axis.tlast  <= s_axis.tlast;
             m_axis.tdata  <= s_axis.tdata;
-            m_axis.tkeep  <= s_axis.tkeep;
-            m_axis.tid    <= s_axis.tid;
-            m_axis.tdest  <= s_axis.tdest;
-            m_axis.tstrb  <= s_axis.tstrb;
-            m_axis.tuser  <= s_axis.tuser;
         end
     end
 
