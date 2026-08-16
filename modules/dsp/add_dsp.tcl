fec72d868178239872c3edcf3ec6245942474e46
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2026-08-16 17:54:10 +0300

    Update add_dsp.tcl

--- modules/dsp/add_dsp.tcl
+++ modules/dsp/add_dsp.tcl
@@ -7,7 +7,6 @@ if {$xilinx == 1} {
     add_files -norecurse $xci_defaultlib
 
     set xil_defaultlib "
-        $path/rtl/fir_filter.sv
         $path/rtl/cmult.sv
         $path/rtl/round.sv
         $path/rtl/sfir_even_symmetric_systolic_top.sv
@@ -28,7 +27,6 @@ if {$xilinx == 1} {
     "
     add_files -fileset sim_1 $xil_defaultlib
 } elseif {$gowin == 1} {
-    add_file $path/rtl/fir_filter.sv
     add_file $path/rtl/cmult.sv
     add_file $path/rtl/round.sv
     add_file $path/rtl/sfir_even_symmetric_systolic_top.sv
