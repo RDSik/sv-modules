286e1a37196a31022e6e38bdd85133a4b6dcd39b
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2026-08-18 18:07:47 +0300

    Update sync_fifo.sv

--- modules/fifo/rtl/sync_fifo.sv
+++ modules/fifo/rtl/sync_fifo.sv
@@ -1,9 +1,8 @@
 /* verilator lint_off TIMESCALEMOD */
 module sync_fifo #(
-    parameter int FIFO_WIDTH   = 32,
-    parameter int FIFO_DEPTH   = 64,
-    parameter int READ_LATENCY = 1,
-    parameter     RAM_STYLE    = "block"
+    parameter int FIFO_WIDTH = 32,
+    parameter int FIFO_DEPTH = 64,
+    parameter     RAM_STYLE  = "block"
 ) (
     input logic clk_i,
     input logic rst_i,
@@ -22,7 +21,7 @@ module sync_fifo #(
     output logic [$clog2(FIFO_DEPTH):0] data_cnt_o
 );
 
-    localparam logic BYPASS_REG_EN = (READ_LATENCY > 0);
+    localparam logic BYPASS_REG_EN = (RAM_STYLE != "distributed");
     localparam int PTR_WIDTH = $clog2(FIFO_DEPTH);
     localparam MAX_PTR = PTR_WIDTH'(FIFO_DEPTH - 1);
 
@@ -95,12 +94,11 @@ module sync_fifo #(
     end
 
     ram_sdp #(
-        .MEM_DEPTH   (FIFO_DEPTH),
-        .BYTE_WIDTH  (FIFO_WIDTH),
-        .BYTE_NUM    (1),
-        .READ_LATENCY(READ_LATENCY),
-        .RAM_STYLE   (RAM_STYLE),
-        .MEM_MODE    ("read_first")
+        .MEM_DEPTH (FIFO_DEPTH),
+        .BYTE_WIDTH(FIFO_WIDTH),
+        .BYTE_NUM  (1),
+        .RAM_STYLE (RAM_STYLE),
+        .MEM_MODE  ("read_first")
     ) i_ram_sdp (
         .a_clk_i  (clk_i),
         .a_en_i   (wr_en),
