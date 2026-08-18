643ef0b4293f364144ed4412b1dfe1ce52602ff5
RDSik <dmitryryabickov@yandex.ru>
2026-08-19 01:09:11 +0300

    add bram_ctrl ip

--- modules/bd/add_bd.tcl
+++ modules/bd/add_bd.tcl
@@ -3,6 +3,11 @@ set path [file dirname [info script]]
 if {$xilinx == 1} {
     source $path/scripts/zynq_bd.tcl
 
+    set xci_defaultlib "
+        $path/ip/axi_bram_ctrl_0/axi_bram_ctrl_0.xci
+    "
+    add_files -norecurse $xci_defaultlib
+
     set xil_defaultlib "
         $path/rtl/bd_top.sv
     "
