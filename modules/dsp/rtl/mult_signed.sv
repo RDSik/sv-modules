0ca1cfc309b383add66ba6dabf206f7315dd3c31
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2026-08-20 17:41:57 +0300

    Update mult_signed.sv

--- modules/dsp/rtl/mult_signed.sv
+++ modules/dsp/rtl/mult_signed.sv
@@ -1,4 +1,7 @@
 /* verilator lint_off TIMESCALEMOD */
+
+// Multiplier with 1 dsp and 4 latency
+
 module mult_signed #(
     parameter int AWIDTH = 16,
     parameter int BWIDTH = 16
@@ -13,16 +16,19 @@ module mult_signed #(
     logic signed [BWIDTH-1:0] b_d, b_dd;
     logic signed [AWIDTH+BWIDTH:0] p_d, p_dd;
 
-    always_ff @(posedge clk) begin
+    always_ff @(posedge clk) begin  // a input dual reg
         a_d  <= a;
         a_dd <= a_d;
+    end
+
+    always_ff @(posedge clk) begin  // b input dual reg
         b_d  <= b;
         b_dd <= b_d;
     end
 
     always_ff @(posedge clk) begin
-        p_d  <= a_dd * b_dd;
-        p_dd <= p_d;
+        p_d  <= a_dd * b_dd;  // mult reg
+        p_dd <= p_d;  // p output reg
     end
 
     assign p = p_dd;
