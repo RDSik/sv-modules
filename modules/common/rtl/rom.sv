d1084ecf9784fff8a7a58348cadc35a00f5837b4
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2026-08-21 14:34:31 +0300

    Update rom.sv

--- modules/common/rtl/rom.sv
+++ modules/common/rtl/rom.sv
@@ -13,22 +13,22 @@ module rom #(
     output logic [        MEM_WIDTH-1:0] data_o
 );
 
-    (* rom_style = ROM_STYLE *) logic [MEM_WIDTH-1:0] ram[MEM_DEPTH];
+    (* rom_style = ROM_STYLE *) logic [MEM_WIDTH-1:0] rom[MEM_DEPTH];
 
     initial begin
-        $readmemh(MEM_FILE, ram);
+        $readmemh(MEM_FILE, rom);
     end
 
     logic [MEM_WIDTH-1:0] data;
 
     if (ROM_STYLE == "distributed") begin : g_distributed_ram
-        assign data_o = ram[addr_i];
+        assign data_o = rom[addr_i];
     end else if (ROM_STYLE == "block") begin : g_other_ram
         always_ff @(posedge clk_i) begin
             if (rst_i) begin
                 data <= '0;
             end else if (en_i) begin
-                data <= ram[addr_i];
+                data <= rom[addr_i];
             end
         end
     end else begin : g_ram_style_err
