44301dc51a7eb436f522f637960e1ba8b75aa214
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2026-08-21 13:12:06 +0300

    Update round.sv

--- modules/dsp/rtl/round.sv
+++ modules/dsp/rtl/round.sv
@@ -1,13 +1,16 @@
 /* verilator lint_off TIMESCALEMOD */
+
+// Convergent Rounding (LSB Correction Technique)
+
 module round #(
-    parameter int CH_NUM         = 2,
-    parameter int DATA_WIDTH_IN  = 32,
-    parameter int DATA_WIDTH_OUT = 16
+    parameter int   CH_NUM         = 2,
+    parameter int   DATA_WIDTH_IN  = 32,
+    parameter int   DATA_WIDTH_OUT = 16,
+    parameter logic ROUND_TYPE     = 1,     // 1 - even, 0 - odd
+    parameter       USE_DSP        = "yes"
 ) (
     input logic clk_i,
     input logic rst_i,
-
-    input logic round_type_i,
 
     input logic [CH_NUM-1:0][DATA_WIDTH_IN-1:0] tdata_i,
     input logic                                 tvalid_i,
@@ -21,33 +24,32 @@ module round #(
         assign tvalid_o = tvalid_i;
     end else begin : g_round
         localparam int FRAC_WIDTH = DATA_WIDTH_IN - DATA_WIDTH_OUT;
-        
+
         for (genvar i = 0; i < CH_NUM; i++) begin : g_ch
-            logic signed [DATA_WIDTH_IN-1:0] data_in;
-            assign data_in = signed'(tdata_i[i]);
-    
             logic [FRAC_WIDTH-1:0] add;
-            assign add = (round_type_i) ? {1'b1, {{FRAC_WIDTH - 1} {1'b0}}} : {1'b0, {{FRAC_WIDTH - 1} {1'b1}}};
-    
-            logic signed [DATA_WIDTH_IN-1:0] sum_reg;
-            logic signed [DATA_WIDTH_IN-1:0] sum;
-            assign sum = data_in + {{DATA_WIDTH_OUT{1'b0}}, add};
-    
+            assign add = (ROUND_TYPE) ? {1'b1, {{FRAC_WIDTH - 1} {1'b0}}} : {1'b0, {{FRAC_WIDTH - 1} {1'b1}}};
+
             logic [FRAC_WIDTH-1:0] pattern;
-            logic                  pattern_detect;
-    
-            assign pattern = (round_type_i) ? {FRAC_WIDTH{1'b0}} : {FRAC_WIDTH{1'b1}};
-    
-            always_ff @(posedge clk_i) begin
-                pattern_detect <= (sum[FRAC_WIDTH-1:0] == pattern);
+            assign pattern = (ROUND_TYPE) ? {FRAC_WIDTH{1'b0}} : {FRAC_WIDTH{1'b1}};
+
+            (* use_dsp = USE_DSP *) logic signed [DATA_WIDTH_IN-1:0] sum_reg; // pre_adder reg
+            logic signed [DATA_WIDTH_IN-1:0] sum; // pre_adder 
+            logic signed [DATA_WIDTH_IN-1:0] data_in; // input reg
+            logic pattern_detect; // pattern_detect
+            
+            always_ff @(posedge clk_i) begin // dsp
+                data_in        <= signed'(tdata_i[i]);
                 sum_reg        <= sum;
+                pattern_detect <= (sum[FRAC_WIDTH-1:0] == pattern);
             end
-    
+
+            assign sum = data_in + {{DATA_WIDTH_OUT{1'b0}}, add}; // pre_adder 
+
             logic signed [DATA_WIDTH_OUT-1:0] data_out;
-    
+
             always_ff @(posedge clk_i) begin
                 if (pattern_detect) begin
-                    data_out <= {sum_reg[DATA_WIDTH_IN-1:FRAC_WIDTH+1], ~round_type_i};
+                    data_out <= {sum_reg[DATA_WIDTH_IN-1:FRAC_WIDTH+1], ~ROUND_TYPE};
                 end else begin
                     data_out <= sum_reg[DATA_WIDTH_IN-1:FRAC_WIDTH];
                 end
@@ -55,16 +57,19 @@ module round #(
 
             assign tdata_o[i] = data_out;
         end
-    
-        logic tvalid_d;
-    
-        always_ff @(posedge clk_i) begin
-            if (rst_i) begin
-                {tvalid_o, tvalid_d} <= '0;
-            end else begin
-                {tvalid_o, tvalid_d} <= {tvalid_d, tvalid_i};
-            end
-        end
+
+        shift_reg #(
+            .DATA_WIDTH($bits(tvalid_i)),
+            .DELAY     (3),
+            .RESET_EN  (1),
+            .SRL_STYLE ("register")
+        ) i_shift_reg (
+            .clk_i (clk_i),
+            .rst_i (rst_i),
+            .en_i  (1'b1),
+            .data_i(tvalid_i),
+            .data_o(tvalid_o)
+        );
     end
 
 endmodule
